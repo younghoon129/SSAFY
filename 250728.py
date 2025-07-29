e47e3440c67e1a0e1ba4c54fac24894afd74bf3e
yonunghoon <yhkor11@naver.com>
2025-07-29 17:13:00 +0900

    250729.제어문,조건문,반복문,함수와 스코프,리스트 컴프리핸션

---
+++ 250728.py
@@ -0,0 +1,13 @@
+# print("hello world")
+# print(-2 ** 4)
+# number = 10
+# double = number * 2
+# print(double)
+
+# number = 5
+# double = number * 2
+# print(double)
+
+a = 1
+b = 1.0
+print(a == b)
