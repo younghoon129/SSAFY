f11b1e969071be5afc729c15c00a9b061361ca31
ehtjd9995 <ehtjd9995@naver.com>
2025-08-20 15:12:05 +0900

    modified file stack2

---
+++ 2508/250812_스택2/250812_후위표기식변환함수.py
@@ -0,0 +1,10 @@
+# 중위 표기식을 후위 표기식으로 변환하는 함수
+def infix_to_postfix(expression):
+    # 연산자의 우선순위
+    op_dict = {'+' : 1, '-': 1, '*': 2, '/': 2, '(': 0}
+    stack = []  # 연산자를 저장할 스택
+    postfix = []  # 후위 표기식을 저장할 리스트
+
+    # 주어진 입력값을 하나하나씩 순회하면서 보자
+    for ch in expression:
+        #
