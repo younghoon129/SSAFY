e5ff3a2a23493076adc27d04112d07e8eb024bf1
김영훈 <yhkor11@naver.com>
2025-11-04 19:38:03 +0900

    modified file - square

--- 보충수업/2511_IM대비2_codetree/251104_codetree_square/square.py
+++ 보충수업/2511_IM대비2_codetree/251104_codetree_square/square.py
@@ -1,6 +1,5 @@
 import sys
-sys.stdin = open('codetree_square_input.txt', 'r')
-n = int(input())
+sys.stdin = open('C:\\Users\\user\\Desktop\\코딩폴더 깃허브\\김영훈\\SSAFY\\보충수업\\2511_IM대비2_codetree\\251104_codetree_square\\codetree_square_input.txt', 'r')
 from pprint import pprint
 # x1, y1, x2, y2 = [], [], [], []
 # for _ in range(n):
@@ -9,14 +8,16 @@ from pprint import pprint
 #     y1.append(b)
 #     x2.append(c)
 #     y2.append(d)
+n = int(input())
+
 box = [[0] * 200 for _ in range(200)]
 for _ in range(n):
     a, b, c, d = map(int, input().split())
-    for i in range(d-b + 99):
-        for j in range(c-a + 99):
+    for i in range((b+ 99), (d + 99)):
+        for j in range((a + 99), (c + 99)):
             box[i][j] = 1
 result = 0
 for i in range(len(box)):
     result += sum(box[i])
-print(box)
+# print(box)
 print(result)
